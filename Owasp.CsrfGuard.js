8d0f77a9b8070439a7371cef4e6651d75574c581
Jeanne <nyjeanne@gmail.com>
2013-02-09 10:05:23 -0500

    Update Owasp.CsrfGuard.js
    partial IE support

--- Owasp.CsrfGuard.js
+++ Owasp.CsrfGuard.js
@@ -1,191 +1,140 @@
-/**
- * The OWASP CSRFGuard Project, BSD License
- * Eric Sheridan (eric@infraredsecurity.com), Copyright (c) 2011 
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *    1. Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *    2. Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *    3. Neither the name of OWASP nor the names of its contributors may be used
- *       to endorse or promote products derived from this software without specific
- *       prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- /** Changed line 320 per "BUG FIX" described there.
-*/
-(function() {
-  /**
-	 * Code to ensure our event always gets triggered when the DOM is updated.
-	 * @param obj
-	 * @param type
-	 * @param fn
-	 * @source http://www.dustindiaz.com/rock-solid-addevent/
-	 */
-	function addEvent( obj, type, fn ) {
-	    if (obj.addEventListener) {
-	        obj.addEventListener( type, fn, false );
-	        EventCache.add(obj, type, fn);
-	    }
-	    else if (obj.attachEvent) {
-	        obj["e"+type+fn] = fn;
-	        obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
-	        obj.attachEvent( "on"+type, obj[type+fn] );
-	        EventCache.add(obj, type, fn);
-	    }
-	    else {
-	        obj["on"+type] = obj["e"+type+fn];
-	    }
+// rolledback to owasp csrf guard 2 since 3 is failing so much in IE
+
+
+	var tokenName = '%TOKEN_NAME%';
+	var tokenValue = '%TOKEN_VALUE%';
+
+	function updateAnchors()
+	{
+		updateTag('a','href');
 	}
 	
-	var EventCache = function(){
-	    var listEvents = [];
-	    return {
-	        listEvents : listEvents,
-	        add : function(node, sEventName, fHandler){
-	            listEvents.push(arguments);
-	        },
-	        flush : function(){
-	            var i, item;
-	            for(i = listEvents.length - 1; i >= 0; i = i - 1){
-	                item = listEvents[i];
-	                if(item[0].removeEventListener){
-	                    item[0].removeEventListener(item[1], item[2], item[3]);
-	                };
-	                if(item[1].substring(0, 2) != "on"){
-	                    item[1] = "on" + item[1];
-	                };
-	                if(item[0].detachEvent){
-	                    item[0].detachEvent(item[1], item[2]);
-	                };
-	                item[0][item[1]] = null;
-	            };
-	        }
-	    };
-	}();
+	function updateLinks()
+	{
+		updateTag('link', 'href');
+	}
 	
-	/** string utility functions **/
-	String.prototype.startsWith = function(prefix) {
-		return this.indexOf(prefix) === 0;
-	};
-
-	String.prototype.endsWith = function(suffix) {
-		return this.match(suffix+"$") == suffix;
-	};
-
-	/** hook using standards based prototype **/
-	function hijackStandard() {
-		XMLHttpRequest.prototype._open = XMLHttpRequest.prototype.open;
-		XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
-			this.url = url;
-			
-			this._open.apply(this, arguments);
-		};
+	function updateAreas()
+	{
+		updateTag('area', 'href');
+	}
+	
+	function updateFrames()
+	{
+		updateTag('frame', 'src');
+	}
+	
+	function updateIFrames()
+	{
+		updateTag('iframe', 'src');
+	}
+	
+	function updateStyles()
+	{
+		updateTag('style', 'src');
+	}
+	
+	function updateScripts()
+	{
+		updateTag('script', 'src');
+	}
+	
+	function updateImages()
+	{
+		updateTag('img', 'src');
+	}
+	
+	function updateForms()
+	{
+		var pageTokens = {};
+		var forms = document.getElementsByTagName('form');
 		
-		XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send;
-		XMLHttpRequest.prototype.send = function(data) {
-			if(this.onsend != null) {
-				this.onsend.apply(this, arguments);
+		for(i=0; i<forms.length; i++)
+		{
+			//var html = forms[i].innerHTML;
+			
+			//html += '<input type=hidden name=' + tokenName + ' value=' + tokenValue + ' />';
+			
+			//alert('new html: ' + html);
+			
+			//forms[i].innerHTML = html;
+			
+			injectTokenAttribute(forms[i], "action", tokenName, tokenValue, pageTokens);
+			
+			// added action update from CSRF 3 (needed for posting) - with IE friendly change for adding name to node
+			
+			// hack to test if action is a string since IE returns [object] when action in form and as hidden field
+			// if not a string, assume it is our action and add token for now
+			var action = forms[i].getAttributeNode("action").nodeValue;
+			if(action != null && isValidUrl(action)) {
+				var uri = parseUri(action);
+				
+				// IE hack from http://stackoverflow.com/questions/1650797/setting-name-of-dom-created-element-fails-in-ie-workaround
+				var hidden;
+			    try {
+			    	hidden = document.createElement('<input type="hidden" name="' + tokenName + '" />');
+			    } catch(e) {
+			    	hidden = document.createElement("input");
+			    	hidden.type = "hidden";
+			    	hidden.name = tokenName;
+			    }
+			    hidden.value = (pageTokens[uri] != null ? pageTokens[uri] : tokenValue);
+				forms[i].appendChild(hidden);
+
 			}
 			
-			this._send.apply(this, arguments);
-		};
+		}
 	}
-
-	/** ie does not properly support prototype - wrap completely **/
-	function hijackExplorer() {
-		var _XMLHttpRequest = window.XMLHttpRequest;
+	
+	function updateTag(name,attr)
+	{
+		var links = document.getElementsByTagName(name);
 		
-		function alloc_XMLHttpRequest() {
-			this.base = _XMLHttpRequest ? new _XMLHttpRequest : new window.ActiveXObject("Microsoft.XMLHTTP");
-		}
+		//alert('length: ' + links.length);
 		
-		function init_XMLHttpRequest() {
-			return new alloc_XMLHttpRequest;
-		}
-		
-		init_XMLHttpRequest.prototype = alloc_XMLHttpRequest.prototype;
-		
-		/** constants **/
-		init_XMLHttpRequest.UNSENT = 0;
-		init_XMLHttpRequest.OPENED = 1;
-		init_XMLHttpRequest.HEADERS_RECEIVED = 2;
-		init_XMLHttpRequest.LOADING = 3;
-		init_XMLHttpRequest.DONE = 4;
-		
-		/** properties **/
-		init_XMLHttpRequest.prototype.status = 0;
-		init_XMLHttpRequest.prototype.statusText = "";
-		init_XMLHttpRequest.prototype.readyState = init_XMLHttpRequest.UNSENT;
-		init_XMLHttpRequest.prototype.responseText = "";
-		init_XMLHttpRequest.prototype.responseXML = null;
-		init_XMLHttpRequest.prototype.onsend = null;
-		
-		init_XMLHttpRequest.url = null;
-		init_XMLHttpRequest.onreadystatechange = null;
-
-		/** methods **/
-		init_XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
-			var self = this;
-			this.url = url;
+		for(i=0; i<links.length; i++)
+		{
+			var src = links[i].getAttribute(attr);
 			
-			this.base.onreadystatechange = function() {
-				try { self.status = self.base.status; } catch (e) { }
-				try { self.statusText = self.base.statusText; } catch (e) { }
-				try { self.readyState = self.base.readyState; } catch (e) { }
-				try { self.responseText = self.base.responseText; } catch(e) { }
-				try { self.responseXML = self.base.responseXML; } catch(e) { }
+			if(src != null && src != '')
+			{
+				//alert('found ' + src + '!');
+			
+				if(isHttpLink(src))
+				{
+					var index = src.indexOf('?');
 				
-				if(self.onreadystatechange != null) {
-					self.onreadystatechange.apply(this, arguments);
+					if(index != -1)
+					{
+						src = src + '&' + tokenName + '=' + tokenValue;
+					}
+					else
+					{
+						src = src + '?' + tokenName + '=' + tokenValue;
+					}
+					
+					//alert('new src ' + src);
+					
+					links[i].setAttribute(attr, src);
 				}
 			}
-			
-			this.base.open(method, url, async, user, pass);
-		};
-		
-		init_XMLHttpRequest.prototype.send = function(data) {
-			if(this.onsend != null) {
-				this.onsend.apply(this, arguments);
-			}
-			
-			this.base.send(data);
-		};
-		
-		init_XMLHttpRequest.prototype.abort = function() {
-			this.base.abort();
-		};
-		
-		init_XMLHttpRequest.prototype.getAllResponseHeaders = function() {
-			return this.base.getAllResponseHeaders();
-		};
-		
-		init_XMLHttpRequest.prototype.getResponseHeader = function(name) {
-			return this.base.getResponseHeader(name);
-		};
-		
-		init_XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
-			return this.base.setRequestHeader(name, value);
-		};
-		
-		/** hook **/
-		window.XMLHttpRequest = init_XMLHttpRequest;
+		}
 	}
-
+	
+	function isHttpLink(src)
+	{
+		var result = 0;
+		
+		if(src.substring(0, 4) == 'http' || src.substring(0, 1) == '/' || src.indexOf(':') == -1)
+		{
+			result = 1;
+		}
+		
+		return result;
+	}
+	
+	// ---------------------------------------------
+	// functions used by csrf 3
 	/** check if valid domain based on domainStrict **/
 	function isValidDomain(current, target) {
 		var result = false;
@@ -205,8 +154,9 @@
 	}
 
 	function getHost(url) {
-		if (/\bMSIE 6/.test(navigator.userAgent) && !window.opera) {
-			url = canonicalizeUrl(url); // damn you, ie6
+		// ie 8 wasn't recognizing hostname
+		if (/\bMSIE/.test(navigator.userAgent) && !window.opera) {
+			url = canonicalizeUrl(url); // damn you, ie6 (and ie 8)
 		}
 		var a =  document.createElement('a');
 		a.href = url;
@@ -267,142 +217,55 @@
 		
 		return uri;
 	}
-
-	/** inject tokens as hidden fields into forms **/
-	function injectTokenForm(form, tokenName, tokenValue, pageTokens) {
-		var action = form.getAttribute("action");
-		
-		if(action != null && isValidUrl(action)) {		
-			var uri = parseUri(action);
-			var hidden = document.createElement("input");
-			
-			hidden.setAttribute("type", "hidden");
-			hidden.setAttribute("name", tokenName);
-			hidden.setAttribute("value", (pageTokens[uri] != null ? pageTokens[uri] : tokenValue));
-
-			form.appendChild(hidden);
-		}
-	}
-
+	
 	/** inject tokens as query string parameters into url **/
-	function injectTokenAttribute(element, attr, tokenName, tokenValue, pageTokens) {
-		var location = element.getAttribute(attr);
+	function injectTokenAttribute(element, attributeName, tokenName, tokenValue, pageTokens) {
+		//var location = element.getAttribute(attributeName);
+		// hack - getting same error as on action - don't know why but hack to move forward
+		var attr = element.getAttributeNode(attributeName);
+		var location = null;
+		if ( attr != null) {
+			location = attr.nodeValue;
+		}
 		
 		if(location != null && isValidUrl(location)) {
 			var uri = parseUri(location);
 			var value = (pageTokens[uri] != null ? pageTokens[uri] : tokenValue);
+			
 			
 			if(location.indexOf('?') != -1) {
 				location = location + '&' + tokenName + '=' + value;
 			} else {
 				location = location + '?' + tokenName + '=' + value;
 			}
+			
 
 			try {
-				element.setAttribute(attr, location);
+				var attr = document.createAttribute(attributeName);
+				attr.nodeValue = location;
+				element.setAttributeNode(attr);
 			} catch (e) {
 				// attempted to set/update unsupported attribute
 			}
 		}
 	}
 
-	/** inject csrf prevention tokens throughout dom **/
-	function injectTokens(tokenName, tokenValue) {
-		/** obtain reference to page tokens if enabled **/
-		var pageTokens = {};
-		
-		if(%TOKENS_PER_PAGE% == true) {
-			pageTokens = requestPageTokens();
-		}
-		
-		/** iterate over all elements and injection token **/
-		var all = document.all ? document.all : document.getElementsByTagName('*');
-		var len = all.length;
-		
-		// BUG FIX: if go through in ascending order, the indexes change as you add hidden elements
-		// when have 100 forms, this moves some of the forms past the original document.all size
-		for(var i=len -1; i>=0; i--) {
-			var element = all[i];
-			
-			/** inject into form **/
-			if(element.tagName.toLowerCase() == "form") {
-				if(%INJECT_FORMS% == true) {
-					injectTokenForm(element, tokenName, tokenValue, pageTokens);
-					injectTokenAttribute(element, "action", tokenName, tokenValue, pageTokens);
-				}
-				/** inject into attribute **/
-			} else if(%INJECT_ATTRIBUTES% == true) {
-				injectTokenAttribute(element, "src", tokenName, tokenValue, pageTokens);
-				injectTokenAttribute(element, "href", tokenName, tokenValue, pageTokens);
-			}
-		}
-	}
 
-	/** obtain array of page specific tokens **/
-	function requestPageTokens() {
-		var xhr = window.XMLHttpRequest ? new window.XMLHttpRequest : new window.ActiveXObject("Microsoft.XMLHTTP");
-		var pageTokens = {};
-		
-		xhr.open("POST", "%SERVLET_PATH%", false);
-		xhr.send(null);
-		
-		var text = xhr.responseText;
-		var name = "";
-		var value = "";
-		var nameContext = true;
-		
-		for(var i=0; i<text.length; i++) {
-			var character = text.charAt(i);
-			
-			if(character == ':') {
-				nameContext = false;
-			} else if(character != ',') {
-				if(nameContext == true) {
-					name += character;
-				} else {
-					value += character;
-				}
-			}
-			
-			if(character == ',' || (i + 1) >= text.length) {
-				pageTokens[name] = value;
-				name = "";
-				value = "";
-				nameContext = true;
-			}
-		}
-		
-		return pageTokens;
+	
+	// ---------------------------------------------
+	
+	// put in function
+	function addCsrfAttributes() {
+	updateAnchors();
+	updateLinks();
+	updateAreas();
+	updateFrames();
+	updateIFrames();
+	//updateStyles();
+	//updateScripts();
+	//updateImages();
+	updateForms();
 	}
 	
-	/**
-	 * Only inject the tokens if the JavaScript was referenced from HTML that
-	 * was served by us. Otherwise, the code was referenced from malicious HTML
-	 * which may be trying to steal tokens using JavaScript hijacking techniques.
-	 */
-	if(isValidDomain(document.domain, "%DOMAIN_ORIGIN%")) {
-		/** optionally include Ajax support **/
-		if(%INJECT_XHR% == true) {
-			if(navigator.appName == "Microsoft Internet Explorer") {
-				hijackExplorer();
-			} else {
-				hijackStandard();
-			}
-			
-			XMLHttpRequest.prototype.onsend = function(data) {
-				if(isValidUrl(this.url)) {
-					this.setRequestHeader("X-Requested-With", "%X_REQUESTED_WITH%")
-					this.setRequestHeader("%TOKEN_NAME%", "%TOKEN_VALUE%");
-				}
-			};
-		}
-		
-		/** update nodes in DOM after load **/
-		addEvent(window,'unload',EventCache.flush);
-		addEvent(window,'load', function() {
-			injectTokens("%TOKEN_NAME%", "%TOKEN_VALUE%");
-		});
-	} else {
-		alert("OWASP CSRFGuard JavaScript was included from within an unauthorized domain!");
-	}
-})();
+	// i know this is bad coding - trying to get IE working before making it better
+	window.onload = addCsrfAttributes;
